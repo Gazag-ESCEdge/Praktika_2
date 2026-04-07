143456abc87256eb5db247648357b664bb556ea5
Артём Ванин <jafj1521@gmail.com>
2026-04-07 23:00:45 +0300

    Add files via upload

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -13,10 +13,33 @@ class Product extends Model
     protected $fillable = [
         'article',
         'name',
+        'brand',
         'description',
+        'complect',
+        'character',
         'price',
-        'image',
-        'category_id'
+        'image1',
+        'image2',
+        'image3',
+        'image4',
+        'image5',
+        'image6',
+        'image7',
+        'image8',
+        'image9',
+        'image10',
+        'image11',
+        'image12',
+        'image13',
+        'image14',
+        'image15',
+        'category_id',
+        'for_order',
+        'reviews_num',
+        'hit',
+        'sale',
+        'rating',
+        'empty'
     ];
 
     public function category()
